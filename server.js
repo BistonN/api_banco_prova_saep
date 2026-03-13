8377d393f358e3b1199f6c789706f311043d3e74
BistonN <biston.nunes@gmail.com>
2026-03-13 14:09:00 -0300

    alterando config do server http

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
-server.listen( process.env.API_PORT || 3000, function () {
+server.listen( process.env.API_PORT || 3000, '0.0.0.0', function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
